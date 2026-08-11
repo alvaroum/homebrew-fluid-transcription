b92eee7e9edb1bad4d47b61be1293475dcc5ff1e
Alvaro Garcia <alvarorgf@icloud.com>
2026-08-11 13:45:03 +0200

    fix: omit redundant formula version

--- Formula/fluid-transcription.rb
+++ Formula/fluid-transcription.rb
@@ -2,7 +2,6 @@ class FluidTranscription < Formula
   desc "Native macOS CLI for transcription, diarization, and media processing"
   homepage "https://github.com/alvaroum/fluid-transcription"
   url "https://github.com/alvaroum/fluid-transcription/releases/download/v202608.1/fluid-transcription-202608.1-macos-arm64.tar.gz"
-  version "202608.1"
   sha256 "184324b0fce2ff17b7a1145a78b374ec1732db5895991091d0c07159f1019865"
   license "Apache-2.0"
   depends_on arch: :arm64
